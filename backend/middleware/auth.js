319012e7681099992ebd4befc035b314c02db7ad
Tanja <tanja.tepponen@hotmail.com>
2026-03-11 22:20:52 +0200

    juups

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,10 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function auth(req, res, next) {
-  console.log("AUTH header:", req.headers.authorization);
-  console.log("JWT_SECRET exists:", !!process.env.JWT_SECRET);
-
-  const header = req.headers.authorization; // "Bearer <token>"
+  const header = req.headers.authorization;
   const token = header && header.startsWith("Bearer ") ? header.slice(7) : null;
 
   if (!token) return res.status(401).json({ message: "Token puuttuu" });
